5a2880cdbd13189bc451aba2faa813d64d2839dc
kevingael69 <lomk080725hgtprva3@cbtis217.edu.mx>
2024-10-17 11:22:38 -0600

    Version 3 con comentario mejorado y simplificado para mejor lectura

--- suma/sume.cpp
+++ suma/sume.cpp
@@ -1,17 +1,17 @@
-#include <bits/stdc++.h>
+#include <bits/stdc++.h> //Primero abri codeblocks para crear el problema para despues declarar 3 variables para poder calcular las hice double para sacar resultados con punto decimal y despues lei las variables para pude usar el codigo y despues puse las formulas a calcular para despues poder imprimir x1 y x2
 using namespace std;
 int main() {
     int R, N, C;
-    double D, x1, x2; //declare variables double para resultados con puntos
+    double D, x1, x2;
 
     cin >> R >> N>> C;
 
     D = (N*N) - (4 * R * C);
 
-    x1 = (-N+ sqrt(D)) / (2 * R); //puse la formula para calcular
+    x1 = (-N+ sqrt(D)) / (2 * R);
     x2 = (-N- sqrt(D)) / (2 * R);
 
-    cout << x1 << " " << x2 << endl; //imprimi x1 y x2
+    cout << x1 << " " << x2 << endl;
     cout<< "Kevin Gael lopez Moreno";
 
     return 0;
