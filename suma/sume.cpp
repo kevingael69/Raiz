a22cc8714de9a01a2dd53eac52bcc7f09fb51ba5
kevingael69 <lomk080725hgtprva3@cbtis217.edu.mx>
2024-10-17 11:20:25 -0600

    Version 2 de codigo con mi nombre y comentarios :D

--- suma/sume.cpp
+++ suma/sume.cpp
@@ -2,17 +2,17 @@
 using namespace std;
 int main() {
     int R, N, C;
-    double D, x1, x2;
-
+    double D, x1, x2; //declare variables double para resultados con puntos
 
     cin >> R >> N>> C;
 
     D = (N*N) - (4 * R * C);
 
-    x1 = (-N+ sqrt(D)) / (2 * R);
+    x1 = (-N+ sqrt(D)) / (2 * R); //puse la formula para calcular
     x2 = (-N- sqrt(D)) / (2 * R);
 
-    cout << x1 << " " << x2 << endl;
+    cout << x1 << " " << x2 << endl; //imprimi x1 y x2
+    cout<< "Kevin Gael lopez Moreno";
 
     return 0;
 }
